93000a093b06ce1d3fd8e9052c26697cbdbc4a4b
Arsonist <adsmi888@gmail.com>
2024-07-10 08:01:04 +0300

    add place for words

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ let rightHand = document.getElementById("right_hand")
 let leftHand = document.getElementById("left_hand")
 let rightLeg = document.getElementById("right_leg")
 let leftLeg = document.getElementById("left_leg")
-
+let emptyWord = document.getElementById('word_container')
 
 
 submitButton.addEventListener('click', () => {
     const wordPlay = inputField.value
-    console.log(wordPlay)
     // wordToPlay.innerHTML = wordPlay
     for (let i = 0; i < deleteItems.length; i++) {
         deleteItems[i].style.display = "none"; 
     }
     for (let i = 0; i < showItems.length; i++) {
         showItems[i].style.display = "block"; 
-    }  
+    }
+    
+    const letters = wordPlay.split("")
+    for (letter in letters) {
+        let emptyCell = document.createElement('span')
+        emptyCell.classList.add("word_letter")
+        emptyCell.innerHTML = letters[letter]
+        emptyCell.style.textTransform = 'uppercase'
+        emptyWord.appendChild(emptyCell)
+    }
+    
 })
 
 head.style.visibility = 'hidden'
