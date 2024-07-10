f81c3dfe7cd0cc43dd7d645390cad257f9b9b204
Arsonist <adsmi888@gmail.com>
2024-07-10 04:12:09 +0300

    remove mistakes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const submitButton = document.getElementById('button_submit')
-const inputField = document.getElementById('input_word');
+const inputField = document.getElementById('input_word')
+const deleteItems = document.getElementsByClassName('page-1')
+let showItems = document.getElementsByClassName('page-2')
+let wordToPlay = document.getElementById("word_to_play")
 
-
-submitButton.addEventListener('click', (evt) => {
-    evt.preventDefault()
-    console.log(inputField.value)
+submitButton.addEventListener('click', () => {
+    const wordPlay = inputField.value
+    console.log(wordPlay)
+    wordToPlay.innerHTML = wordPlay
+    for (let i = 0; i < deleteItems.length; i++) {
+        deleteItems[i].style.display = "none"; 
+    }
+    for (let i = 0; i < showItems.length; i++) {
+        showItems[i].style.display = "block"; 
+    }  
 })
